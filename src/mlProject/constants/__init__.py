9c9bdaa38a2d7d3efcb25a4b8a9170754519b2d6
siddharth Tyagi <sidtyagi05@gmail.com>
2023-09-22 13:29:34 +0530

    data ingestion done

---
+++ src/mlProject/constants/__init__.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+from src.mlProject.logger import logging
+from src.mlProject.exception import CustomException
+
+
+#to read yaml files
+CONFIG_FILE_PATH=Path("config/config.yaml")
+PARAMS_FILE_PATH=Path("params.yaml")
+SCHEMA_FILE_PATH=("schema.yaml")
